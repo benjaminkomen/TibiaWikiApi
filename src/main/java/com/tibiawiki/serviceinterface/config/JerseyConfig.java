d98e90b15da15221ea0db631461d8563cc0fd889
Benjamin Komen <komenb@gmail.com>
2018-08-18 13:03:55 +0200

    Swagger UI support. Removed DefaultResource (as it wasn't needed anyways).

--- src/main/java/com/tibiawiki/serviceinterface/config/JerseyConfig.java
+++ src/main/java/com/tibiawiki/serviceinterface/config/JerseyConfig.java
@@ -5,18 +5,28 @@ import io.swagger.jaxrs.config.BeanConfig;
 import io.swagger.jaxrs.listing.ApiListingResource;
 import io.swagger.jaxrs.listing.SwaggerSerializers;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 @Component
 public class JerseyConfig extends ResourceConfig {
 
+    @Value("${spring.jersey.application-path:/}")
+    private String apiPath;
+
     public JerseyConfig() {
         registerEndpoints();
-        configureSwagger();
+    }
+
+    @PostConstruct
+    public void init() {
+        // Register components where DI is needed
+        this.configureSwagger();
     }
 
     private void registerEndpoints() {
-        register(DefaultResource.class);
         register(AchievementsResource.class);
         register(BooksResource.class);
         register(BuildingsResource.class);
@@ -36,17 +46,19 @@ public class JerseyConfig extends ResourceConfig {
         register(StreetsResource.class);
     }
 
-    public void configureSwagger() {
+    private void configureSwagger() {
         this.register(ApiListingResource.class);
         this.register(SwaggerSerializers.class);
         BeanConfig beanConfig = new BeanConfig();
+        beanConfig.setConfigId("tibiawikiapi");
         beanConfig.setTitle("TibiaWikiApi");
         beanConfig.setVersion("1.2.0");
-        beanConfig.setSchemes(new String[]{"http"});
+        beanConfig.setContact("B. Komen");
+        beanConfig.setSchemes(new String[]{"http", "https"});
+        beanConfig.setBasePath(this.apiPath); // location where dynamically created swagger.json is reachable
         beanConfig.setHost("localhost:8080");
-        beanConfig.setBasePath("/");
         beanConfig.setResourcePackage("com.tibiawiki");
-        beanConfig.setScan(true);
         beanConfig.setPrettyPrint(true);
+        beanConfig.setScan(true); // scan packages via setResourcePackage
     }
 }
