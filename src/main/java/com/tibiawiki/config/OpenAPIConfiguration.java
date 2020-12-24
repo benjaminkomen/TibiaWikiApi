410a9d1868d4f2b95f5ce70056ba94b72c99ee9d
Benjamin Komen <benjamin_komen@hotmail.com>
2020-12-24 12:33:21 +0100

    Specify servers as slash.

--- src/main/java/com/tibiawiki/config/OpenAPIConfiguration.java
+++ src/main/java/com/tibiawiki/config/OpenAPIConfiguration.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
@@ -17,6 +18,7 @@ public class OpenAPIConfiguration {
     public OpenAPI customOpenAPI(@Autowired BuildProperties buildProperties) {
         return new OpenAPI()
                 .components(new Components())
+                .addServersItem(new Server().url("/"))
                 .info(new Info()
                         .title("TibiaWikiApi")
                         .contact(new Contact().name("B. Komen"))
