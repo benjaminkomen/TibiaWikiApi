e1ec02a87cf4c25bc5d6139e7a28df14ee55c5ad
Benjamin Komen <benjamin_komen@hotmail.com>
2021-02-22 17:15:44 +0100

    Whoops.

--- src/main/kotlin/com/tibiawiki/config/OpenAPIConfiguration.kt
+++ src/main/kotlin/com/tibiawiki/config/OpenAPIConfiguration.kt
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.info.License
+import io.swagger.v3.oas.models.servers.Server
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.info.BuildProperties
 import org.springframework.context.annotation.Bean
